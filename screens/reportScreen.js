b7fe7f4bba70399727d02797d684a8ebce37b2f3
fahdaalromi <alromifahda1@gmail.com>
2020-03-14 10:05:18 +0300

    report screen without the shape

--- screens/reportScreen.js
+++ screens/reportScreen.js
@@ -1,313 +1,215 @@
-import React ,{ Component } from 'react';
-import { ScrollView,
-    StyleSheet,
-    Text,
-    View,
-    TextInput,
-    Button,
-    TouchableHighlight,
-    Image,
-    Alert,
-    ImageBackground,
-    TouchableOpacity,
+import * as WebBrowser from 'expo-web-browser';
+import React, { Component } from 'react';
+import {
+  Platform,
+    StyleSheet, Text, View, Image, Button, backgroundColor, Alert, border, WIDTH, TouchableHighlight, 
+    TouchableOpacity, ScrollView, ImageBackground,
 } from 'react-native';
-import { FontAwesome5 ,AntDesign,Feather,MaterialCommunityIcons,SimpleLineIcons} from "@expo/vector-icons";
+import { FontAwesome,FontAwesome5 ,AntDesign,Feather,MaterialCommunityIcons,SimpleLineIcons} from "@expo/vector-icons";
+import { MonoText } from '../components/StyledText';
 import {LinearGradient} from 'expo-linear-gradient';
+import { StackActions } from '@react-navigation/native';
+import { NavigationActions } from 'react-navigation';
 
-export default class profileScreen extends Component {
+export default class reportScreen extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-
+            
         }
     }
 
+    open_profile() {
+        const navigateAction = NavigationActions.navigate({
+            routeName: 'profile',
+            action: NavigationActions.navigate({ routeName: 'profile' }), 
+        });
+    
+        this.props.navigation.dispatch(navigateAction);
+    }
+    
     render() {
         return (
-            <View>
-                <View style={styles.container}>
-
-
-                    <ImageBackground source={require('../assets/images/halfBlue.png') } style={{ height:"100%",justifyContent: 'center',alignItems: 'center', marginBottom:400}}>
-                        <View style={styles.smallContainer}>
-                            <Text style={styles.perInfo}>──── المعلومات الشخصية ────</Text>
-                                <View style={styles.inputContainer}>
-                                    <TextInput style={styles.inputs}
-                                        placeholder="أسم المستخدم"
-                                        keyboardType = 'ascii-capable'
-                                        underlineColorAndroid='transparent'
-                                    />
-                                </View>
-                                <View style={styles.inputContainer}>
-                                    <TextInput style={styles.inputs}
-                                        placeholder="البريد الإلكتروني"
-                                        keyboardType="email-address"
-                                        underlineColorAndroid='transparent'
-                                    />
-                                </View>
-                                <Text style={styles.perInfo}>──── تغيير كلمة المرور  ────</Text>
-                                <View style={styles.inputContainer}>
-                                    <TextInput style={styles.inputs}
-                                        placeholder="كلمة المرور"
-                                        secureTextEntry={true}
-                                        underlineColorAndroid='transparent'
-                                    />
-                                </View>
-                                <View style={styles.inputContainer}>
-                                    <TextInput style={styles.inputs}
-                                        placeholder="تأكيد كلمة المرور"
-                                        secureTextEntry={true}
-                                        underlineColorAndroid='transparent'
-                                    />
-                                </View>
-                                <Text style={styles.perInfo}>────── غيرها    ──────</Text>
-                                <View style={styles.inputContainer}>
-                                    <TextInput style={styles.inputs}
-                                        placeholder="الحد الائتماني للفاتورة"
-                                        secureTextEntry={true}
-                                        underlineColorAndroid='transparent'
-                                    />
-                                </View>
-                                <TouchableHighlight style={[styles.LocationButtonContainer, styles.AddlocationButton]} onPress={()=>{this.props.navigation.navigate('location')}}  >
-                                    <Text style={styles.addLocationText}> إضافة موقع</Text>
-                                </TouchableHighlight>
-
-                                <View>
-                                    <Text style={styles.AnalysisText}>  تحليل التحركات </Text>
-                                </View>
-                                <View style={styles.AnalysisButtonContainer}>
-                                    <TouchableHighlight  style={[styles.AnalysisButton]} >
-                                        <Text style={styles.subAnalysisText} > تفعيل </Text>
-                                    </TouchableHighlight>
-                                </View>
-
-                                <TouchableHighlight style={[styles.buttonContainer, styles.signupButton]} >
-                                    <Text style={styles.signUpText}>  حفظ </Text>
-                                </TouchableHighlight>
-
+            <View style={styles.container}>
+                <ImageBackground source={require('./otherhalf.png')} style={{ width:'100%' , height:'120%', flex: 1, justifyContent: "center", alignItems: "center"}}>
+                    <ScrollView style = {{width: '100%', padding: 20}}>
+                        <View style = {{width: '100%', alignItems: 'flex-end'}}>
+                            <Text style={styles.routineTitle}> إجمالي الإستهلاك </Text>
                         </View>
-                    </ImageBackground>
-                </View>
+                        <View style = {{width: '100%', borderRadius: 10, alignItems: 'center', padding: 15, backgroundColor: '#ffffff', marginTop: 10, marginBottom: 10}}>
+                            <Text style = {styles.contentText}> إذا كنت تريد تفعيل هذة الخاصية يرجى ملء خانة "الحد الإئتماني للفاتورة" </Text>
+                            <TouchableOpacity style = {styles.button_style} onPress = {() => this.open_profile()}>
+                                <Text style = {styles.button_text}> أنقر هنا</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style = {{width: '100%', alignItems: 'flex-end'}}>
+                            <Text style={styles.routineTitle}> تفصيل الإستهلاك </Text>
+                        </View>
+                        <View style = {{width: '100%', borderRadius: 10, alignItems: 'center', padding: 15, paddingBottom: 0, backgroundColor: '#ffffff', marginTop: 10, marginBottom: 10}}>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.8, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ٧٠٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> الإنارة </Text>
+                                </View>
+                            </View>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.4, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ٢٠٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> التلفاز </Text>
+                                </View>
+                            </View>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.1, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ١٠٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> البوابة </Text>
+                                </View>
+                            </View>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.4, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ٥٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> التكييف </Text>
+                                </View>
+                            </View>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.4, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ١٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> باب المنزل </Text>
+                                </View>
+                            </View>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.4, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ٢٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> آلة القهوة </Text>
+                                </View>
+                            </View>
+                            <View style = {styles.component_view}>
+                                <View style = {styles.component_bar_view}>
+                                    <LinearGradient colors = {['#2287ac', '#ffffff']} start = {[0, 0]} end = {[0.4, 0]} style = {styles.component_bar} />
+                                    <Text style = {styles.bar_text}> ٣٥٪ </Text>
+                                </View>
+                                <View style = {styles.component_text_view}>
+                                    <Text style = {styles.contentText}> جهاز الإنترنت </Text>
+                                </View>
+                            </View>
+                        </View>
+                        <View style = {{height: 30}}/>
+                    </ScrollView>
+                </ImageBackground>
             </View>
         );
     }
+    
 }
-profileScreen.navigationOptions = ({navigation})=> ({
 
-  headerTint:'#F7FAFF',
-  headerTitle: 'الملف الشخصي',
+
+
+
+reportScreen.navigationOptions = ({navigation})=> ({
+
+  headerTitle:  'الأجهزة المتصلة',
+  
   headerRight:()=>(
     <TouchableOpacity onPress={()=>{navigation.navigate('Home')}} style={{marginRight:15}}>
       <AntDesign name="right" size={24} color="#CDCCCE" />
     </TouchableOpacity>
-
   ),
   headerLeft:()=>(
+
+
+
     <TouchableOpacity onPress={()=>{navigation.navigate('')}} style={{marginLeft:15}}>
-      <SimpleLineIcons name="logout" size={24} color="#CDCCCE" />
+      <SimpleLineIcons name="logout" size={24} color="#fff" />
     </TouchableOpacity>
   ),
   headerStyle: {
-    backgroundColor: '#4b9cb5',
+    backgroundColor: '#8BC4D0',
     color:'white'
-
+    
  },
  headerTitleStyle: {
   color: '#fff'
 }
-
 });
+
 
 const styles = StyleSheet.create({
 
-
-  header:{
-    marginTop:150,
-    marginLeft:130,
-    color: 'white',
-    fontSize:25,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-
-  container: {
-
-    //flex: 1,
-   justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F7FAFF',
-  },
-
-  backgroundIMG:{
-   flex:1,
-   width:'100%',
-   height:'100%',
-
-  },
-
-  inputContainer: {
-      backgroundColor: '#FFFFFF',
-      borderRadius:30,
-      borderWidth: 1,
-      width:250,
-      height:35,
-      marginBottom:15,
-      bottom: 20,
-      borderColor: '#3E82A7'
-  },
-
-  smallContainer:{
-    margin:70,
-    //marginTop:-160,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderRadius:10,
-      width:300,
-      height:650
-  },
-
-  perInfo:{
-    color: '#3E82A7',
-    fontSize: 20,
-    bottom: 30,
-    marginTop: 20,
-    marginBottom:20,
-  },
-
-  inputs:{
-      //flex:1,
-      height:40,
-      alignSelf:'flex-end',
-      borderColor: '#3E82A7',
-      marginRight:20,
-     //marginLeft:-50,
-
-  },
-
-
-
-  buttonContainer: {
-   height:45,
-   flexDirection: 'row',
-   justifyContent: 'center',
-   alignItems: 'center',
-   marginBottom:10,
-   width:250,
-   borderRadius:30,
-  },
-
-  AnalysisButtonContainer:{
-    height:45,
-    width:70,
- //borderWidth:1,
- marginRight:150,
- marginBottom:10,
- //backgroundColor:'#3E82A7',
- //backgroundColor: this.sate.active?'#3E82A7':'red',
-   //height:45,
-   //flexDirection: 'row',
-   //justifyContent: 'center',
-   //alignItems: 'center',
-   //marginBottom:10,
-   //width:100,
-   borderRadius:20,
-  },
-
-  AnalysisButton:{
-    height:45,
-    width:70,
-   backgroundColor:'#6FA0AF',
-   alignItems:'center',
-   justifyContent:'center',
-   borderRadius:20,
-   //left:this.state.active ? 50 : 0
-   //marginRight:150,
-  },
-
-  signupButton: {
-   backgroundColor: "#3E82A7",
-
-  },
-
-  LocationButtonContainer:{
-
-   height:45,
-   //flexDirection: 'row',
-   justifyContent: 'center',
-   alignItems: 'center',
-   marginTop:10,
-   width:'85%',
-   borderRadius:45,
-   borderColor:'#6FA0AF',
-   borderWidth:1,
-  },
-
-  AddlocationButton: {
-   backgroundColor: "#ffffff",
-   marginTop:-20,
-   marginBottom:20,
-
- },
-
- addLocationText:{
-   color: '#6FA0AF',
-   fontSize:15,
- },
-
-  signUpText: {
-    color: 'white',
-    fontSize:15,
-  },
-
-  AnalysisText:{
-   color: '#6FA0AF',
-   marginLeft:150,
-   marginBottom:-200,
-   marginTop:10,
-
-  },
-  subAnalysisText:{
-    color: 'white',
-    //fontSize:15,
-  },
-
-  inline:{
-   //flex:1,
-   flexDirection:'row',
-   justifyContent:'center',
-   //marginRight:50,
-   //marginLeft:50,
-
-
-  },
-
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#F7FAFF'
+    },
+    routineTitle: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#2287ac',
+    },
+    contentText: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: '#2287ac',
+    },
+    button_style: {
+        width: 200,
+        height: 40,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#2287ac',
+        marginTop: 20
+    },
+    button_text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#ffffff',
+    },
+    component_view: {
+       width: '100%',
+       marginBottom: 10,
+       flexDirection: 'row',
+       justifyContent: 'space-around',
+       alignItems: 'center'
+    },
+    component_bar_view: {
+        flex: 1, 
+        marginRight: 10,
+        justifyContent: 'center'
+    },
+    component_text_view: {
+        width: 100, 
+        justifyContent: 'flex-end'
+    },
+    component_bar: {
+        width: '100%',
+        height: 10,
+        borderWidth: 0.5,
+        borderColor: '#2287ac'
+    },
+    bar_text: {
+        fontSize: 14,
+        textAlign: 'center',
+        color: '#2287ac',
+    }
 });
